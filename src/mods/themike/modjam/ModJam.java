a7586ecbf71e402d599d86bc4f33ac87b12b7056
TrainerGuy22 <rocketkid1@gmail.com>
2013-03-28 17:47:17 -0400

    Started base of mod.

---
+++ src/mods/themike/modjam/ModJam.java
@@ -0,0 +1,35 @@
+package mods.themike.modjam;
+
+import cpw.mods.fml.common.Mod;
+import cpw.mods.fml.common.Mod.Init;
+import cpw.mods.fml.common.Mod.Instance;
+import cpw.mods.fml.common.Mod.PostInit;
+import cpw.mods.fml.common.Mod.PreInit;
+import cpw.mods.fml.common.event.FMLInitializationEvent;
+import cpw.mods.fml.common.event.FMLPostInitializationEvent;
+import cpw.mods.fml.common.event.FMLPreInitializationEvent;
+import cpw.mods.fml.common.network.NetworkMod;
+
+@NetworkMod(clientSideRequired = true, serverSideRequired = false)
+@Mod(modid = "ModJamMagic!", name = "ModJam - With Magic!", version = "1")
+public class ModJam {
+
+	@Instance("ModJamMagic!")
+	public ModJam instance;
+	
+	@PreInit
+	public void preInit(FMLPreInitializationEvent event) {
+		
+	}
+	
+	@Init
+	public void init(FMLInitializationEvent event) {
+		
+	}
+	
+	@PostInit
+	public void postInit(FMLPostInitializationEvent event) {
+		
+	}
+	
+}
