754a68adb00f060685a76f2dc86996ce57794fb7
TrainerGuy22 <rocketkid1@gmail.com>
2013-03-28 23:00:20 -0400

    Going to bed, not going to work on it.

--- src/mods/themike/modjam/ModJamRecipies.java
+++ src/mods/themike/modjam/ModJamRecipies.java
@@ -7,9 +7,7 @@ import cpw.mods.fml.common.registry.GameRegistry;
 public class ModJamRecipies {
 	
 	public static void init() {
-		
 		GameRegistry.addRecipe(new ItemStack(ModJam.item, 1, 1), new Object[]{"XXX", "XIX", "XXX", 'X', new ItemStack(ModJam.item, 1, 0), 'I', Item.ingotGold});
-		
 	}
 
 }
