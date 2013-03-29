6eca577ca0822f6fe5f0e353100f1593939c736f
TrainerGuy22 <rocketkid1@gmail.com>
2013-03-29 19:05:08 -0400

    Puush.

---
+++ src/mods/themike/modjam/gui/GuiPapyrusScroll.java
@@ -0,0 +1,17 @@
+package mods.themike.modjam.gui;
+
+import net.minecraft.client.gui.inventory.GuiContainer;
+import net.minecraft.inventory.Container;
+
+public class GuiPapyrusScroll extends GuiContainer {
+
+	public GuiPapyrusScroll(Container par1Container) {
+		super(par1Container);
+	}
+
+	@Override
+	protected void drawGuiContainerBackgroundLayer(float f, int i, int j) {
+		
+	}
+
+}
