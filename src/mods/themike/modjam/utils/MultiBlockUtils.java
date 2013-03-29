daee6b3d47f953ba30f67613010ed3bf0f62dc91
TrainerGuy22 <rocketkid1@gmail.com>
2013-03-29 17:27:02 -0400

    Back, and puuushing Magebrick.

--- src/mods/themike/modjam/utils/MultiBlockUtils.java
+++ src/mods/themike/modjam/utils/MultiBlockUtils.java
@@ -2,7 +2,7 @@ package mods.themike.modjam.utils;
 
 public class MultiBlockUtils {
 	
-	protected static String[] decoration = new String[]{"magestone"};
+	protected static String[] decoration = new String[]{"magebrick"};
 	
 	public static String[] getArray() {
 		return decoration;
