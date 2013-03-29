2d85f06caf5e06020f19e1fcf7b30d7711df4cd8
TrainerGuy22 <rocketkid1@gmail.com>
2013-03-28 21:14:32 -0400

    Finished up ItemMulti.

--- src/mods/themike/modjam/items/ItemMulti.java
+++ src/mods/themike/modjam/items/ItemMulti.java
@@ -1,8 +1,11 @@
 package mods.themike.modjam.items;
 
+import java.util.List;
+
 import cpw.mods.fml.relauncher.Side;
 import cpw.mods.fml.relauncher.SideOnly;
 import net.minecraft.client.renderer.texture.IconRegister;
+import net.minecraft.creativetab.CreativeTabs;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
 import net.minecraft.util.Icon;
@@ -10,12 +13,13 @@ import net.minecraft.util.Icon;
 public class ItemMulti extends Item {
 	
 	private static final String[] subNames = new String[]{"dustSpacial"};
-	private static Icon[] subIcons = new Icon[]{};
+	private static Icon[] subIcons = new Icon[subNames.length];
 	
 	public ItemMulti(int par1) {
 		super(par1);
 		this.hasSubtypes = true;
 		this.setUnlocalizedName("miscItems");
+		this.setCreativeTab(CreativeTabs.tabMisc);
 	}
 	
 	@Override
@@ -27,10 +31,22 @@ public class ItemMulti extends Item {
 	}
 	
 	@Override
+	public Icon getIconFromDamage(int metadata) {
+		return subIcons[metadata];
+	}
+	
+	@Override
 	@SideOnly(Side.CLIENT)
 	public void updateIcons(IconRegister reg) {
 		for(int par1 = 0; par1 < subNames.length; par1++) {
 			subIcons[par1] = reg.registerIcon("mikejam:" + subNames[par1]);
+		}
+	}
+	
+	@Override
+	public void getSubItems(int ID, CreativeTabs tabs, List list) {
+		for(int par1 = 0; par1 < subNames.length; par1++) {
+			list.add(new ItemStack(ID, 1, par1));
 		}
 	}
 	
