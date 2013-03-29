46d6048a589a3d353f69c466db484f94e06e58a7
TrainerGuy22 <rocketkid1@gmail.com>
2013-03-28 20:38:20 -0400

    ItemMulti work.

--- src/mods/themike/modjam/items/ItemMulti.java
+++ src/mods/themike/modjam/items/ItemMulti.java
@@ -1,11 +1,24 @@
 package mods.themike.modjam.items;
 
 import net.minecraft.item.Item;
+import net.minecraft.item.ItemStack;
 
 public class ItemMulti extends Item {
+	
+	private static final String[] subNames = new String[]{"dustSpacial"};
 
 	public ItemMulti(int par1) {
 		super(par1);
+		this.hasSubtypes = true;
+		this.setUnlocalizedName("miscItems");
+	}
+	
+	@Override
+	public String getUnlocalizedName(ItemStack stack) {
+		if(stack.getItemDamage() >= 0 && stack.getItemDamage() < subNames.length) {
+			return this.getUnlocalizedName() + subNames[stack.getItemDamage()];
+		}
+		return null;
 	}
 
 }
