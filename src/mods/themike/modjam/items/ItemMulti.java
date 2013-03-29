c2f73d269d6a33a5593fca23241659391bf1a259
TrainerGuy22 <rocketkid1@gmail.com>
2013-03-28 20:58:52 -0400

    Icon work.

--- src/mods/themike/modjam/items/ItemMulti.java
+++ src/mods/themike/modjam/items/ItemMulti.java
@@ -1,12 +1,17 @@
 package mods.themike.modjam.items;
 
+import cpw.mods.fml.relauncher.Side;
+import cpw.mods.fml.relauncher.SideOnly;
+import net.minecraft.client.renderer.texture.IconRegister;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
+import net.minecraft.util.Icon;
 
 public class ItemMulti extends Item {
 	
 	private static final String[] subNames = new String[]{"dustSpacial"};
-
+	private static Icon[] subIcons = new Icon[]{};
+	
 	public ItemMulti(int par1) {
 		super(par1);
 		this.hasSubtypes = true;
@@ -20,5 +25,13 @@ public class ItemMulti extends Item {
 		}
 		return null;
 	}
-
+	
+	@Override
+	@SideOnly(Side.CLIENT)
+	public void updateIcons(IconRegister reg) {
+		for(int par1 = 0; par1 < subNames.length; par1++) {
+			subIcons[par1] = reg.registerIcon("mikejam:" + subNames[par1]);
+		}
+	}
+	
 }
