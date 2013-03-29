8a83c9ab8ccfb67163a12917d8bf1db86a1c5267
TrainerGuy22 <rocketkid1@gmail.com>
2013-03-29 10:45:35 -0400

    Recipe!

--- src/mods/themike/modjam/ModJamRecipies.java
+++ src/mods/themike/modjam/ModJamRecipies.java
@@ -1,5 +1,6 @@
 package mods.themike.modjam;
 
+import net.minecraft.block.Block;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
 import cpw.mods.fml.common.registry.GameRegistry;
@@ -8,6 +9,7 @@ public class ModJamRecipies {
 	
 	public static void init() {
 		GameRegistry.addRecipe(new ItemStack(ModJam.item, 1, 1), new Object[]{"XXX", "XIX", "XXX", 'X', new ItemStack(ModJam.item, 1, 0), 'I', Item.ingotGold});
+		GameRegistry.addRecipe(new ItemStack(ModJam.runes, 1, 0), new Object[]{"XX ", "XXX", " XX", 'X', Block.stone});
 	}
 
 }
