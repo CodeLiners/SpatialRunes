72b74b46b2685daac3a51e40bbdc954cdf99ea39
TrainerGuy22 <rocketkid1@gmail.com>
2013-03-30 14:40:18 -0400

    Gotta puush.

--- src/mods/themike/modjam/gui/GuiPapyrusScroll.java
+++ src/mods/themike/modjam/gui/GuiPapyrusScroll.java
@@ -13,12 +13,12 @@ public class GuiPapyrusScroll extends GuiContainer {
 	@Override
 	protected void drawGuiContainerBackgroundLayer(float f, int i, int j) {
 		this.mc.renderEngine.bindTexture("/mods/mikejam/textures/gui/parchment.png");
-		this.drawTexturedModalRect((this.xSize + 82) / 2, (this.ySize - 110) / 2, 0, 0, 187, 177);
+		this.drawTexturedModalRect((this.xSize + 75) / 2, (this.ySize - 125) / 2, 0, 0, 187, 177);
 	}
 	
 	@Override
 	protected void drawGuiContainerForegroundLayer(int x, int y) {
-		this.drawCenteredString(this.mc.fontRenderer, "Papyrus Scroll", (xSize - fontRenderer.getStringWidth("Papyrus Scroll") / 2), 6, ColorUtils.applyColor(9));
+		this.drawString(fontRenderer, "Papyrus Scroll", (xSize - fontRenderer.getStringWidth("Papyrus Scroll")) / 2, 12, 4210752);
 	}
 
 }
