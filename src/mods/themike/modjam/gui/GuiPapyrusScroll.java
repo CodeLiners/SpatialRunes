c68bad082557413ccea268171171b02ab8a8f29b
TrainerGuy22 <rocketkid1@gmail.com>
2013-03-30 15:22:18 -0400

    Animations!

--- src/mods/themike/modjam/gui/GuiPapyrusScroll.java
+++ src/mods/themike/modjam/gui/GuiPapyrusScroll.java
@@ -1,5 +1,7 @@
 package mods.themike.modjam.gui;
 
+import org.lwjgl.opengl.GL11;
+
 import mods.themike.modjam.utils.ColorUtils;
 import net.minecraft.client.gui.inventory.GuiContainer;
 import net.minecraft.inventory.Container;
@@ -13,14 +15,19 @@ public class GuiPapyrusScroll extends GuiContainer {
 	@Override
 	protected void drawGuiContainerBackgroundLayer(float f, int i, int j) {
 		this.mc.renderEngine.bindTexture("/mods/mikejam/textures/gui/parchment.png");
-		this.drawTexturedModalRect((this.xSize + 75) / 2, (this.ySize - 125) / 2, 0, 0, 187, 177);
+		int x = (this.width - 187) / 2;
+		int y = (this.height - 200) / 2;
+		this.drawTexturedModalRect(x, y, 0, 0, 187, 177);
 	}
 	
 	@Override
 	protected void drawGuiContainerForegroundLayer(int x, int y) {
 		this.fontRenderer.drawString("Papyrus Scroll", (xSize - fontRenderer.getStringWidth("Papyrus Scroll")) / 2, 12, 0000000, false);
+		GL11.glPushMatrix();
+		GL11.glColor4f(1.0F, 1.0F, 1.0F, 1.0F);
 		this.mc.renderEngine.bindTexture("/mods/mikejam/textures/gui/parts.png");
-		this.drawTexturedModalRect(305, 20, 0, 0, 22, 15);
+		this.drawTexturedModalRect(150, 130, 0, 0, 22, 14);
+		GL11.glPopMatrix();
 	}
 
 }
