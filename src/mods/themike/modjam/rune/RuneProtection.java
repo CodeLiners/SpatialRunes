ceccee50ff98452193c1de9fc751b98d6f66ab91
jameslfc19 <jameslfc19@gmail.com>
2013-04-01 13:40:33 +0100

    New Rune

---
+++ src/mods/themike/modjam/rune/RuneProtection.java
@@ -0,0 +1,35 @@
+package mods.themike.modjam.rune;
+
+import net.minecraft.entity.player.EntityPlayer;
+import net.minecraft.item.Item;
+import net.minecraft.potion.Potion;
+import net.minecraft.potion.PotionEffect;
+
+public class RuneProtection implements IRune {
+	
+	@Override
+	public void onUse(EntityPlayer player) {
+		player.addPotionEffect((new PotionEffect(Potion.resistance.getId(), 150, 5)));
+	}
+
+	@Override
+	public String getName() {
+		return "protection";
+	}
+
+	@Override
+	public int getLevel() {
+		return 15;
+	}
+
+	@Override
+	public int getUses() {
+		return 3;
+	}
+
+	@Override
+	public Object getSacrifice() {
+		return Item.ingotIron;
+	}
+
+}
