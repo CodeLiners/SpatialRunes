0dabe81e4058d72e1a6a0e795423fce3867a1ce5
max96at <max96at@gmail.com>
2013-04-03 00:06:26 +0300

    Added SideOnly Annotation to the ClientProxy.

--- src/mods/themike/modjam/proxy/ClientProxy.java
+++ src/mods/themike/modjam/proxy/ClientProxy.java
@@ -5,7 +5,11 @@ import mods.themike.modjam.render.RenderCarvingStone;
 import mods.themike.modjam.tile.TileEntityCarvingStone;
 import cpw.mods.fml.client.registry.ClientRegistry;
 import cpw.mods.fml.client.registry.RenderingRegistry;
+import cpw.mods.fml.common.Side;
+import cpw.mods.fml.common.asm.SideOnly;
 
+
+@SideOnly(Side.CLIENT)
 public class ClientProxy implements IProxy {
 	
 	public static SimpleBlockRenderingHandler handler = new SimpleBlockRenderingHandler();
